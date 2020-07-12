cbd8474d6fd3ee861f2286ad582c3cc3a95b3eb0
Maaz <maaznadeem246@gmail.com>
2020-07-12 20:15:25 +0500

    created the header without responsiveness

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+
+
+
+
 function App() {
   return (
     <div >
-        Shoe Store app
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/newreleases" element={<div>New Releases</div>} />
+          <Route path="/sale" element={<div>Sale</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
